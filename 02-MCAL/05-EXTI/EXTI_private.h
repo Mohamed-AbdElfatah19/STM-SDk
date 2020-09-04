dcdee23b024f0e3e2a7a3118f2d589cd3ee6d2a3
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-04 17:58:09 +0200

    Update EXTI_private.h

--- 02-MCAL/05-EXTI/EXTI_private.h
+++ 02-MCAL/05-EXTI/EXTI_private.h
@@ -9,12 +9,12 @@
 #define EXTI_PRIVATE_H
 /* DEFINE registers of EXTI  */
 typedef struct{
- volatile u32 IMR ; 
- volatile u32 EMR ;
- volatile	u32		RTSR;
- volatile	u32		FTSR;
- volatile	u32		SWIER;
- volatile	u32		PR;
+ volatile u32   IMR ; 
+ volatile u32   EMR ;
+ volatile	u32	 	RTSR;
+ volatile	u32		 FTSR;
+ volatile	u32		 SWIER;
+ volatile	u32		 PR;
 }EXTI_Type ;
 
 /* define base adress of EXTI registers  */
