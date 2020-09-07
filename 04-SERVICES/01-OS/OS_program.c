503fc29f4e19c0913784c4dde0425f00aa12d859
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-07 12:00:34 +0200

    Update OS_program.c

--- 04-SERVICES/01-OS/OS_program.c
+++ 04-SERVICES/01-OS/OS_program.c
@@ -8,7 +8,29 @@
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
 
+#include "STK_interface.h"
+
 #include "OS_interface.h"
 #include "OS_private.h"
 #include "OS_config.h"
 
+#define NULL (void *)0
+
+static TASK OS_Tasks[NUMBER_OF_TASKS] = {NULL};
+
+
+void SOS_voidCreateTask(u8 Copy_u8ID , u16 Copy_u16Periodicity , void (*ptr) (void) )
+{
+  
+  OS_Tasks[Copy_u8ID].periodicity = Copy_u16Periodicity ;
+  OS_Tasks[Copy_u8ID].Fptr = ptr  ;
+  
+  
+}
+void SOS_voidStart (void)   
+{
+  
+  
+  
+  
+}
