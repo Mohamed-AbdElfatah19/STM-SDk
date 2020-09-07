bb15d9825697d6c5c71f9ad64e709ab735af345c
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-07 11:40:01 +0200

    Update OS_config.h

--- 04-SERVICES/01-OS/OS_config.h
+++ 04-SERVICES/01-OS/OS_config.h
@@ -1,1 +1,14 @@
+/*****************************************/
+/* Author  :  mohamed abd elfatah        */
+/* Version :  V01                        */
+/* Date    :  4 sep 2020             */
+/*****************************************/
 
+#ifndef OS_CONFIG_H
+#def  OS_CONFIG_H
+
+#define NUMBER_OF_TASKS 4
+
+
+
+#endif
