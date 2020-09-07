d8b075d2580f89c97991c48c8174f030e3a27f89
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-07 11:48:27 +0200

    Update OS_private.h

--- 04-SERVICES/01-OS/OS_private.h
+++ 04-SERVICES/01-OS/OS_private.h
@@ -1,9 +1,17 @@
+/*****************************************/
+/* Author  :  mohamed abd elfatah        */
+/* Version :  V01                        */
+/* Date    :  4 sep 2020             */
+/*****************************************/
 
 
-
-
+#ifndef OS_PRIVATE_H
+#def OS_PRIVATE_H
 
 typedef struct {
  u16 periodicity ;
   void (*Fptr) (void) ;
 }Task ;
+
+
+#endif
