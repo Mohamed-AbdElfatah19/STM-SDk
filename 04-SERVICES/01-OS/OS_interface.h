c4cc3d4bdea6f5307d4dec60750fa9721f4c71cc
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-07 14:27:15 +0200

    Update OS_interface.h

--- 04-SERVICES/01-OS/OS_interface.h
+++ 04-SERVICES/01-OS/OS_interface.h
@@ -6,12 +6,20 @@
 
 
 #ifndef OS_INTERFACE_H
-#def OS_INTERFACE_H
+#define OS_INTERFACE_H
 
 
 void SOS_voidCreateTask(u8 Copy_u8ID , u16 Copy_u16Periodicity , void (*ptr) (void)  , u8 Copy_u8FirstDelay);
 
 void SOS_voidStart (void)   ;
+
+void SOS_voidSuspendTask (u8 Copy_u8ID) ;
+
+void SOS_voidResumeTask  (u8 Copy_u8ID) ;
+
+void SOS_voidDeleteTask (u8 Copy_u8ID)  ;  
+
+u8 SOS_u8GetTaskstate (u8 Copy_u8ID)   ;
 
 
 
