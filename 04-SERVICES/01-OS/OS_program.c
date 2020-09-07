0f3455abb8984407e9ca0f4a66007c42f206a175
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-07 11:52:23 +0200

    Update OS_program.c

--- 04-SERVICES/01-OS/OS_program.c
+++ 04-SERVICES/01-OS/OS_program.c
@@ -1,1 +1,14 @@
+/*****************************************/
+/* Author  :  mohamed abd elfatah        */
+/* Version :  V01                        */
+/* Date    :  4 sep 2020             */
+/*****************************************/
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "OS_interface.h"
+#include "OS_private.h"
+#include "OS_config.h"
 
