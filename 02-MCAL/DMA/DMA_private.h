54ad9fc3eb0141b6c6f0d77557a94f5d48c94418
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-14 11:47:44 +0200

    Update DMA_private.h

--- 02-MCAL/DMA/DMA_private.h
+++ 02-MCAL/DMA/DMA_private.h
@@ -1,1 +1,30 @@
+*****************************************/
+/* Author  :  mohamed abd elfatah           */
+/* Version :  V01                          */
+/* Date    : 12 Sep 2020                   */
+/*****************************************/
 
+  
+  #ifndef DMA_PRIVATE_H
+  #define DMA_PRIVATE_H
+  
+  /* definations for Registers */
+  
+  typedef struct {
+   volatile u32 CCR   ;
+   volatile u32 CNDTR ;
+   volatile u32 CPAR  ;
+   volatile u32 CMAR  ;
+   volatile u32 Reserved ;
+  }DMA_channel ;
+  
+
+  typedef struct {
+   volatile u32 ISR          ;  
+   volatile u32 IFCR         ;
+   DMA_channel channel[7]    ;
+  }DMA_t ;
+  
+#define DMA_BASE_ADDRESS ((volatile DMA_t*)0x040020000)
+  
+  #endif 
