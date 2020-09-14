4beecc631fbdaa66fdbf151587aeb9ec1d8343aa
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-14 11:49:31 +0200

    Update DMA_interface.h

--- 02-MCAL/DMA/DMA_interface.h
+++ 02-MCAL/DMA/DMA_interface.h
@@ -1,1 +1,16 @@
+*****************************************/
+/* Author  :  mohamed abd elfatah           */
+/* Version :  V01                          */
+/* Date    : 12 Sep 2020                   */
+/*****************************************/
+  
+  
+  #ifndef DMA_INTERFACE_H
+  #define DMA_INTERFACE_H
+  
+ void MDMA_voidChannelInit (void) ;
 
+ 
+  
+  
+  #endif 
