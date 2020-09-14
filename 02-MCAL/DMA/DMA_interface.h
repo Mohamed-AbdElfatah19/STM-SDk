7338e2e242b2ec2d81a460d1e5c6797e784188e0
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-14 11:53:20 +0200

    Update DMA_interface.h

--- 02-MCAL/DMA/DMA_interface.h
+++ 02-MCAL/DMA/DMA_interface.h
@@ -10,7 +10,6 @@
   
  void MDMA_voidChannelInit (void) ;
 
- 
-  
+ void MDMA_voidChannelStart(u32 * Copy_Pu32SourceAddress , u32 * Copy_Pu32DestinationAddress , u16 Copy_u16BlockLength) ;  
   
   #endif 
