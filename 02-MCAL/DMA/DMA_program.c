d514596b6c1e96c43ac08999cf03a308d8cd0904
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-14 12:19:48 +0200

    Update DMA_program.c

--- 02-MCAL/DMA/DMA_program.c
+++ 02-MCAL/DMA/DMA_program.c
@@ -1,1 +1,20 @@
 
+void MDMA_voidChannelInit (void) 
+{
+  #if CHANNEL_ID == 1 
+    DMA_BASE_ADDRESS->channel[CHANNEL_ID].CCR = ;
+  
+}
+
+ void MDMA_voidChannelStart(u8 Copy_u8ChannelId ,u32 * Copy_Pu32SourceAddress , u32 * Copy_Pu32DestinationAddress , u16 Copy_u16BlockLength) 
+ {
+   /* make sure channel is disabled */
+   CLR_BIT(DMA_BASE_ADDRESS->channel[Copy_u8ChannelId].CCR , 0)                    ;
+   /* write the address */
+   DMA_BASE_ADDRESS->channel[Copy_u8ChannelId].CPAR = Copy_Pu32SourceAddress       ;
+   DMA_BASE_ADDRESS->channel[Copy_u8ChannelId].CMAR = Copy_Pu32DestinationAddress  ;
+   /* write block length */
+   DMA_BASE_ADDRESS->channel[Copy_u8ChannelId].CNDTR = Copy_u16BlockLength         ;
+   /*enable channel */
+   SET_BIT(DMA_BASE_ADDRESS->channel[Copy_u8ChannelId].CCR , 0)                    ;
+ }
