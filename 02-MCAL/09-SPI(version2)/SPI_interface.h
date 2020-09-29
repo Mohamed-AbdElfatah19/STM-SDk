de2c0b499e0a14347bee910017e45ea937f984df
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-29 15:19:21 +0200

    Update SPI_interface.h

--- 02-MCAL/09-SPI(version2)/SPI_interface.h
+++ 02-MCAL/09-SPI(version2)/SPI_interface.h
@@ -11,5 +11,54 @@ typedef enum {
 	SPI2,
 	MAX_TIMERS
 }SPI_Ch_t;
-  
+ 
+/*** Configuration parameters ***/
+typedef struct {
+	/* 1 for master  /   0 for slave  */
+	u8  u8Mode;
+	
+	/* 1 Idle is high  /   0 Idle is low  */
+	u8  u8CPOL ;
+	
+	/* 1 Read first  /   0 Write first  */
+	u8  u8CPHA ;
+	
+	/* 1 for LSB first  /   0 for MSB first  */
+	u8  u8FrameFormat ;
+	
+	/* 1 for 16-bit  /   0 for 8-bit  */
+	u8  u8FrameSize   ;
+	
+	/* 1  S.W mange slave  /   0  H.W mange slave */
+	u8  u8SSM ;
+	
+	/* 1 for Rexieve only  /   0 for Full duplex  */
+	u8  u8TransmissonMode ;
+	
+	/*  (F / 2   )   ->   0   */
+	/*  (F / 4   )   ->   1   */
+	/*  (F / 8   )   ->   2   */
+	/*  (F / 16  )   ->   3   */
+	/*  (F / 32  )   ->   4   */
+	/*  (F / 64  )   ->   5   */
+	/*  (F / 128 )   ->   6   */
+	/*  (F / 256 )   ->   7   */
+	u8  u8Prescaler;
+	
+	/* 1 for Enable  /   0 for Disable  */
+	u8 u8InterruptEnable ;
+}SPI_config_t;
+
+Pre conditions : 
+		1 - Enable RCC for used GPIO pins 
+		2 - Enable RCC for use SPI
+		3 - Configure pins of SPI as output and input
+		4 - Enable NVIC in case of using interrupts	
+		
+u8 SPI_u8ConfigureCh(SPI_Ch_t copy_channel , SPI_config_t *copy_config);
+
+u16 SPI_voidSendRecSynch (SPI_Ch_t copy_channel , u16 copy_u16SendData);
+
+void SPI_voidSendRecAsynch (SPI_Ch_t copy_channel , u16 copy_u16SendData , void (*func)(u16));
+
 #endif  
