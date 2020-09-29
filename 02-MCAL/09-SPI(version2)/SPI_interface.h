836ee8d8095cff0e24e916120fc655a7d6754a27
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-29 15:14:44 +0200

    Update SPI_interface.h

--- 02-MCAL/09-SPI(version2)/SPI_interface.h
+++ 02-MCAL/09-SPI(version2)/SPI_interface.h
@@ -1,1 +1,15 @@
-
+*****************************************************/
+ /* Author      : mohamed abd elfatah                */
+ /* Version     : v01                                */
+ /* date        : 16/9/2020                          */
+/*****************************************************/
+#ifndef SPI_INTERFACE_H
+#define SPI_INTERFACE_H
+  
+typedef enum {
+	SPI1,
+	SPI2,
+	MAX_TIMERS
+}SPI_Ch_t;
+  
+#endif  
