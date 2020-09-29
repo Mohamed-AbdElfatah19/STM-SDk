843535fbc44f2ac58ca236cb71cf1fa61fca2f72
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-29 12:33:47 +0200

    Update SPI_private.h

--- 02-MCAL/09-SPI/SPI_private.h
+++ 02-MCAL/09-SPI/SPI_private.h
@@ -32,5 +32,37 @@ typedef struct
 /*#define MSPI_2_BASE_ADDRESS   0X40005800
 #define MSPI2                ((volatile SPI_t * const)(SPI_2_BASE_ADDRESS))*/
 
+//Bits Definition
+//CR1 REG
+#define CPHA       0
+#define CPOL       1
+#define MSTR       2
+#define BR0        3
+#define SPE        6
+#define LSBFIRST   7
+#define SSI        8
+#define SSM        9
+#define RXONLY     10
+#define DFF        11
+#define CRCNEXT    12
+#define CRCEN      13
+#define BIDIOE     14
+#define BIDIMODE   15
+//CR2 REG
+#define RXDMAEN    0
+#define TXDMAEN    1
+#define SSOE       2
+#define ERRIE      5
+#define RXNEIE     6
+#define TXEIE      7
+//SR REG
+#define RXNE       0
+#define TXE        1
+#define CHSIDE     2
+#define UDR        3
+#define CRCERR     4
+#define MODF       5
+#define OVR        6
+#define BSY        7
 
 #endif 
