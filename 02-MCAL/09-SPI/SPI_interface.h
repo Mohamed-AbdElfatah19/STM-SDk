e856e19c19bf5bec7a261ff92b3b6f308f0e3b3f
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-29 12:55:17 +0200

    Update SPI_interface.h

--- 02-MCAL/09-SPI/SPI_interface.h
+++ 02-MCAL/09-SPI/SPI_interface.h
@@ -20,10 +20,10 @@
 
 void MSPI_voidInit(void);
 
-u16  MSPI1_voidSendReceiveSync(u8 Copy_u8SlaveNumber, u16 Copy_u16DataToTransimit);
-void MSPI1_voidSendReceiveAsync(u8 Copy_u8SlaveNumber, u16 Copy_u16DataToTransimit, void (*CallBack)(u8 Copy_u8DataToReceive));
+void  MSPI1_voidSendReceiveSync(u8 Copy_u16DataToTransimit , u8 Copy_u16DataToReceive );
+void  MSPI1_voidSendReceiveAsync(u8 Copy_u16DataToTransimit , u8 Copy_u16DataToReceive);
 
-u16  MSPI2_voidSendReceiveSync(u8 Copy_u8SlaveNumber, u16 Copy_u16DataToTransimit);
-void MSPI2_voidSendReceiveAsync(u8 Copy_u8SlaveNumber, u16 Copy_u16DataToTransimit, void (*CallBack)(u8 Copy_u8DataToReceive));
+void MSPI2_voidSendReceiveSync(u8 Copy_u16DataToTransimit , u8 Copy_u16DataToReceive);
+void MSPI2_voidSendReceiveAsync(u8 Copy_u16DataToTransimit , u8 Copy_u16DataToReceive);
 
 #endif 
