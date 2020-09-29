1612f74c807906508cd44ce7daf940d437f3e5d0
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-09-29 12:03:53 +0200

    Update SPI_program.c

--- 02-MCAL/09-SPI/SPI_program.c
+++ 02-MCAL/09-SPI/SPI_program.c
@@ -1,1 +1,20 @@
-
+***************************************************************************
+* Title                 :   SPI   
+* Filename              :   SPI_config.h
+* Author                :   Mohamed abd elfatah 
+* Origin Date           :   16/09/2020
+* Version               :   1.0.0
+*****************************************************************************/
+  
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "GPIO_interface.h"
+#include "SPI_interface.h"
+#include "SPI_config.h"
+#include "SPI_private.h"
+  
+void MSPI_voidInit(void)
+{
+ #if  SPI_1_MODE == SPI_MODE_MASTER 
+  
+}
