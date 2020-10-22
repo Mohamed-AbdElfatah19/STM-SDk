7c83b0cfde32a3807f01623993488dba95b714c4
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2020-10-22 15:20:16 +0200

    Update DMA_private.h

--- 02-MCAL/DMA/DMA_private.h
+++ 02-MCAL/DMA/DMA_private.h
@@ -16,13 +16,13 @@
    volatile u32 CPAR  ;
    volatile u32 CMAR  ;
    volatile u32 Reserved ;
-  }DMA ;
+  }DMA_channel ;
   
 
   typedef struct {
    volatile u32 ISR          ;  
    volatile u32 IFCR         ;
-   DMA channel[7]    ;
+   DMA_channel channel[7]    ;
   }DMA_t ;
   
 #define MDMA ((volatile DMA_t*)0x40020000)
